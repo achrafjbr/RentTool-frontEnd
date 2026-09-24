3e39202a346f94770a6139f02b601adf37b91ec6
achrafjbr <developerjbr@gmail.com>
2026-09-24 20:49:44 +0200

    fix vite env variables in docker build

--- src/pages/GuestPage.tsx
+++ src/pages/GuestPage.tsx
@@ -7,8 +7,8 @@ import ToolCompoenet from "../features/tool/componenets/ToolCompoenet";
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 import { getAllTools, getToolCities } from "../features/tool/toolThunks";
 import Loader from "../components/common/Loader";
-// import type { FailureResponse } from "../types/failureResoponse";
-// import toast from "react-hot-toast";
+import type { FailureResponse } from "../types/failureResoponse";
+import toast from "react-hot-toast";
 import { categories } from "../utilis/constants";
 import ToolCitySelect from "../features/tool/componenets/ToolCitySelect";
 import { motion } from "motion/react";
@@ -39,25 +39,13 @@ export default function GuestPage() {
   const { tools, isLoading, cities } = useAppSelector((state) => state.tool);
 
   useEffect(() => {
-    console.log("TOOL PAGE MOUNTED");
-
-    dispatch(getAllTools())
-      .unwrap()
-      .then((data) => {
-        console.log("GET ALL TOOLS SUCCESS:", data);
-      })
-      .catch((error) => {
-        console.error("GET ALL TOOLS ERROR:", error);
-      });
-
-    dispatch(getToolCities())
-      .unwrap()
-      .then((data) => {
-        console.log("GET TOOL CITIES SUCCESS:", data);
-      })
-      .catch((error) => {
-        console.error("GET TOOL CITIES ERROR:", error);
-      });
+    try {
+      (() => dispatch(getAllTools()).unwrap())();
+      (() => dispatch(getToolCities()).unwrap())();
+    } catch (error) {
+      const err = error as FailureResponse;
+      toast.error(err.message);
+    }
   }, [dispatch]);
 
   const filterSearch = useMemo(() => {
